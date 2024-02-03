6090f6f7b5d694a36f9ced28260318a63842425a
Moriah Obaje <mobaje20@gmail.com>
2024-02-03 15:24:05 +0000

    Add project files.

---
+++ Models/Doctor.cs
@@ -0,0 +1,9 @@
+﻿namespace HospitalBedManagement.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public int AssignedPatients { get; set; } = 0;
+        public string Title { get; set; }
+    }
+}
